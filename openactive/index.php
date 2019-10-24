bffeaf84ef38a4e089ffb345e2264ac1bedce1fe
Marco Marassi <thtg88@users.noreply.github.com>
2019-10-24 15:06:09 +0100

    Data is now constructed as an associative array

--- openactive/index.php
+++ openactive/index.php
@@ -5,8 +5,31 @@ require "../vendor/autoload.php";
 use OpenActive\DatasetSiteTemplate\TemplateRenderer;
 
 // Get JSON-LD data
-$json = file_get_contents("../example.json");
-$data = json_decode($json, true);
 
-// Render compiled template with JSON-LD data
 echo (new TemplateRenderer())->render($data);
+// Create new dataset
+$data = array(
+    "backgroundImageUrl" => "https://ourparks.org.uk/bg.jpg",
+    // TODO: Where does bookingBaseUrl go?
+    "bookingBaseUrl" => "https://ourparks.org.uk/openbooking/",
+    "datasetSiteDiscussionUrl" => "https://github.com/ourparks/opendata",
+    "datasetSiteUrl" => "https://ourparks.org.uk/openactive",
+    "email" => "hello@ourparks.org.uk",
+    "includeCourseInstanceFeed" => false,
+    "includeEventFeed" => false,
+    "includeScheduledSessionFeed" => true,
+    "includeSessionSeriesFeed" => true,
+    "legalEntity" => "Our Parks",
+    "name" => "Our Parks Sessions",
+    "openDataBaseUrl" => "https://ourparks.org.uk/opendata/",
+    "organisationLogoUrl" => "https://ourparks.org.uk/logo.png",
+    "organisationName" => "Our Parks",
+    "organisationUrl" => "https://ourparks.org.uk/",
+    "plainTextDescription" => "Our Parks - turn up tone up!",
+    // TODO: should documentationUrl, platformName, and platformUrl be a parameter?
+    "documentationUrl" => "https://ourparks.org.uk/openbooking/",
+    "platformName" => "AcmeBooker",
+    "platformUrl" => "https://acmebooker.example.com/",
+);
+
+// Render compiled template with data
