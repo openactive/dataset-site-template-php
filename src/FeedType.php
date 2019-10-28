c8a5aafeb3a598fa6e8158ca8d36a00952e8ce59
Marco Marassi <thtg88@users.noreply.github.com>
2019-10-28 15:22:42 +0000

    Added EventSeries to FeedType enum

--- src/FeedType.php
+++ src/FeedType.php
@@ -7,6 +7,7 @@ class FeedType
     const COURSE = "Course";
     const COURSE_INSTANCE = "CourseInstance";
     const EVENT = "Event";
+    const EVENT_SERIES = "EventSeries";
     const FACILITY_USE = "FacilityUse";
     const HEADLINE_EVENT = "HeadlineEvent";
     const INDIVIDUAL_FACILITY_USE = "IndividualFacilityUse";
