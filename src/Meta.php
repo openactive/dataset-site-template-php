4f2a1e69102d649b507c79f443fb55f4baaffdb9
Marco Marassi <thtg88@users.noreply.github.com>
2019-10-28 13:07:28 +0000

    Fixed RPDE media type

--- src/Meta.php
+++ src/Meta.php
@@ -4,5 +4,5 @@ namespace OpenActive\DatasetSiteTemplate;
 
 final class Meta
 {
-    const RPDE_MEDIA_TYPE = "application/vnd.openactive+json; model=2.0, booking=1.0, rpde=1.0";
+    const RPDE_MEDIA_TYPE = "application/vnd.openactive.rpde+json; version=1";
 }
