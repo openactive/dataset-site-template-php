87824a3648f9ebef50e4f2c6f131c7b324138c52
nickevansuk <2616208+nickevansuk@users.noreply.github.com>
2022-07-13 19:32:48 +0100

    Fix case

--- openactive/index.php
+++ openactive/index.php
@@ -26,12 +26,12 @@ $settings = array(
     "platformUrl" => "https://acmebooker.example.com/",
     "backgroundImageUrl" => "https://data.better.org.uk/images/bg.jpg",
     "dateFirstPublished" => "2019-10-28",
-    "OpenBookingAPIBaseUrl" => "https://reference-implementation.openactive.io/api/openbooking",
-    "OpenBookingAPIAuthenticationAuthorityUrl" => "https://auth.reference-implementation.openactive.io",
-    "OpenBookingAPIDocumentationUrl" => "https://developer.openactive.io/go/open-booking-api",
-    "OpenBookingAPITermsOfServiceUrl" => "https://example.com/api-terms-page",
-    "OpenBookingAPIRegistrationUrl" => "https://example.com/api-landing-page",
-    "TestSuiteCertificateUrl" => "https://certificates.reference-implementation.openactive.io/examples/all-features/controlled/"
+    "openBookingAPIBaseUrl" => "https://reference-implementation.openactive.io/api/openbooking",
+    "openBookingAPIAuthenticationAuthorityUrl" => "https://auth.reference-implementation.openactive.io",
+    "openBookingAPIDocumentationUrl" => "https://developer.openactive.io/go/open-booking-api",
+    "openBookingAPITermsOfServiceUrl" => "https://example.com/api-terms-page",
+    "openBookingAPIRegistrationUrl" => "https://example.com/api-landing-page",
+    "testSuiteCertificateUrl" => "https://certificates.reference-implementation.openactive.io/examples/all-features/controlled/"
 );
 
 $feedTypes = array(
